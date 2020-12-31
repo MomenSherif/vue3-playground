484586d77b60ddd0f103366db8cd883d85820d93
MomenSherif <momensherif.2019@gmail.com>
2020-12-31 23:34:30 +0200

    attribute binding

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const app = Vue.createApp({
   data() {
     return {
+      url: 'https://www.google.com',
       books: [
         { title: '📘', author: '🐱‍👤' },
         { title: '📗', author: '🐱‍🐉' },

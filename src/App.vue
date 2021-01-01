ca38e3679ab5914876e40b27963ca61788eeb3eb
MomenSherif <momensherif.2019@gmail.com>
2021-01-01 15:04:39 +0200

    🎉 INT COMMIT: Vue Cli

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{ title }}</h1>
 </template>
 
 <script>
@@ -8,13 +7,15 @@ import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return {
+      title: 'Hello, World! 🌍'
+    }
   }
 }
 </script>
 
-<style>
+<style >
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -22,5 +23,11 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+h1 {
+  display: inline-block;
+  padding-bottom: 10px;
+  border-bottom: 1px solid #ddd;
 }
 </style>

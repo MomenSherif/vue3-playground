7bf806590a25345d1345c86facc17b1ba19126be
MomenSherif <momensherif.2019@gmail.com>
2021-01-01 15:13:49 +0200

    template refs

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
   <h1>{{ title }}</h1>
+  <input type="text" ref="name">
+  <button @click="handleClick">Click me</button>
 </template>
 
 <script>
@@ -11,6 +13,13 @@ export default {
     return {
       title: 'Hello, World! 🌍'
     }
+  },
+  methods: {
+    handleClick(){
+      console.log(this.$refs.name);
+      this.$refs.name.classList.add('active');
+      this.$refs.name.focus();
+    },
   }
 }
 </script>

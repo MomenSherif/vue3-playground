14912b860dc025361405e134de8548bd9a45ac6d
MomenSherif <momensherif.2019@gmail.com>
2021-01-05 23:53:56 +0200

    fetch data in setup

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <PostList v-if="showPosts" :posts="posts" />
-    <button @click="showPosts = !showPosts">toggle posts</button>
+    <PostList v-if="posts.length" :posts="posts" />
+    <div v-if="error">{{ error }}</div>
   </div>
 </template>
 
@@ -14,23 +14,27 @@ export default {
   name: "Home",
   components: { PostList },
   setup() {
-    const posts = ref([
-      {
-        id: 1,
-        title: "Welcome to the blog",
-        body:
-          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis neque aperiam amet. Possimus earum tenetur odio facere unde nobis fuga suscipit. Dolorum neque veniam error dicta repellat deserunt nostrum aliquam culpa quo, fugit quae blanditiis officia molestiae earum corporis incidunt necessitatibus autem harum dolores illum ad! Suscipit maiores similique recusandae accusamus. Minima vero nam iure nisi, totam eius veniam et odit a ipsum soluta nulla asperiores ducimus illum, harum porro explicabo. Nesciunt quod sit aliquam rem laudantium, quibusdam accusantium optio. Repellendus quisquam veritatis nulla dolores. Nihil alias animi ut, doloremque provident sint in asperiores repudiandae dicta velit, fugiat minima ipsum!",
-      },
-      {
-        id: 2,
-        title: "Top 5 CSS tips",
-        body:
-          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis neque aperiam amet. Possimus earum tenetur odio facere unde nobis fuga suscipit. Dolorum neque veniam error dicta repellat deserunt nostrum aliquam culpa quo, fugit quae blanditiis officia molestiae earum corporis incidunt necessitatibus autem harum dolores illum ad! Suscipit maiores similique recusandae accusamus. Minima vero nam iure nisi, totam eius veniam et odit a ipsum soluta nulla asperiores ducimus illum, harum porro explicabo. Nesciunt quod sit aliquam rem laudantium, quibusdam accusantium optio. Repellendus quisquam veritatis nulla dolores. Nihil alias animi ut, doloremque provident sint in asperiores repudiandae dicta velit, fugiat minima ipsum!",
-      },
-    ]);
+    const posts = ref([]);
+    const error = ref(null);
 
-    const showPosts = ref(true);
-    return { posts, showPosts };
+    const load = async () => {
+      try {
+        const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+        if (!res.ok) {
+          throw Error("No data available 😭");
+        }
+        const data = await res.json();
+        posts.value = data;
+      } catch (err) {
+        error.value = err.message;
+      }
+    };
+
+    load();
+    return {
+      posts,
+      error,
+    };
   },
 };
 </script>

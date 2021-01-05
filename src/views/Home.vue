14a41ba54ce65ae40c1baff7579c106de7f956cf
MomenSherif <momensherif.2019@gmail.com>
2021-01-05 13:39:54 +0200

    template ref

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,24 +1,29 @@
 <template>
   <div class="home">home</div>
-  <p>{{ name }} is {{ age }} years old!</p>
+  <p ref="p">{{ name }} is {{ age }} years old!</p>
   <button @click="handleClick">Click</button>
 </template>
 
 <script>
+import { ref, renderList } from "vue";
+
 export default {
   name: "Home",
   setup() {
     const name = "Momen";
     const age = 24;
 
+    const p = ref(null);
+
     const handleClick = () => {
-      console.log("Clicked me ");
+      console.log(p, p.value);
     };
 
     return {
       name,
       age,
       handleClick,
+      p,
     };
   },
 };

ec446461d00208841668b72e27cc00289cb5c226
MomenSherif <momensherif.2019@gmail.com>
2021-01-06 00:08:58 +0200

    Composable functions | like custom hooks

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,34 +2,19 @@
   <div class="home">
     <h1>Home</h1>
     <PostList v-if="posts.length" :posts="posts" />
-    <div v-if="error">{{ error }}</div>
+    <!-- <div v-if="error">{{ error }}</div> -->
   </div>
 </template>
 
 <script>
-import { ref } from "vue";
 import PostList from "../components/PostList";
+import getPosts from "../composables/getPosts";
 
 export default {
   name: "Home",
   components: { PostList },
   setup() {
-    const posts = ref([]);
-    const error = ref(null);
-
-    const load = async () => {
-      try {
-        const res = await fetch("https://jsonplaceholder.typicode.com/posts");
-        if (!res.ok) {
-          throw Error("No data available 😭");
-        }
-        const data = await res.json();
-        posts.value = data;
-      } catch (err) {
-        error.value = err.message;
-      }
-    };
-
+    const { posts, error, load } = getPosts();
     load();
     return {
       posts,

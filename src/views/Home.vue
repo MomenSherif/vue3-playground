df3785567a432a04c6c71363baa5338e581f0e0e
MomenSherif <momensherif.2019@gmail.com>
2021-01-05 13:52:29 +0200

    using refs to reactivity

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,21 +2,23 @@
   <div class="home">home</div>
   <p ref="p">{{ name }} is {{ age }} years old!</p>
   <button @click="handleClick">Click</button>
+  <button @click="age++">age +</button>
+  <input type="text" v-model="name" />
 </template>
 
 <script>
-import { ref, renderList } from "vue";
+import { ref } from "vue";
 
 export default {
   name: "Home",
   setup() {
-    const name = "Momen";
-    const age = 24;
+    const name = ref("Momen");
+    const age = ref(24);
 
     const p = ref(null);
 
     const handleClick = () => {
-      console.log(p, p.value);
+      name.value = "luigi";
     };
 
     return {

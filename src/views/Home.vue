2841b09e65d457976f7551131dd819f8dc431d05
MomenSherif <momensherif.2019@gmail.com>
2021-01-05 13:26:16 +0200

    basics

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,25 @@
 <template>
-  <div class="home"></div>
+  <div class="home">home</div>
+  <p>{{ name }} is {{ age }} years old!</p>
+  <button @click="handleClick">Click</button>
 </template>
 
 <script>
 export default {
   name: "Home",
+  setup() {
+    const name = "Momen";
+    const age = 24;
+
+    const handleClick = () => {
+      console.log("Clicked me ");
+    };
+
+    return {
+      name,
+      age,
+      handleClick,
+    };
+  },
 };
 </script>
